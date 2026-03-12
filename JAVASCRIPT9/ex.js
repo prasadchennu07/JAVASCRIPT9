84c57bfdaaac35feaa5a4b7b3ac3b3d3767423b4
prasadchennu07 <prasadchennu596@gmail.com>
2026-03-12 12:20:13 +0530

    Add files via upload

---
+++ JAVASCRIPT9/ex.js
@@ -0,0 +1,78 @@
+/*i=0;
+while(i<=29){
+    console.log(i)
+    i++
+}
+
+
+i=0;
+do{
+    console.log(i)
+    i++
+}while(i<=29)*/
+
+
+/* let x=22;
+let y=7;
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y); */
+
+
+/* let x = 29;
+let y = 7;
+let operator = "%";
+switch(operator) {
+    case "+":
+        console.log(x + y);
+        break;
+    case "-":
+        console.log(x - y);
+        break;
+    case "/":
+        console.log(x / y);
+        break;
+    case "*":
+        console.log(x * y);
+        break;
+    case "%":
+        console.log(x % y);
+        break;
+    default:
+        console.log(x);
+} */
+
+
+/* let x=29;
+if(x>10){
+    console.log("x is greater than 10");
+}
+else{
+    console.log("x is less than 10");
+} */
+
+
+/* let age=29;
+d=false;
+if(age>18 || d==true){
+    console.log("You are eligible to drive");
+}
+else{
+    console.log("You are not eligible to drive");
+} */
+
+
+let aadhar=true;
+let pan=false;
+if(aadhar){
+    console.log("You can open a bank account in our bank:using aadhar");
+}
+else if (pan) {
+    console.log("You can open a bamk account in our bank:using pan");
+}
+else{
+    console.log("You can't open a bank account in our bank");
+    
+}
